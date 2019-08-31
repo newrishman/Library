328e8ae2e03a65b2740a2bef894d55b1225980c5
newrishman <safarovrishat@gmail.com>
2019-08-31 04:23:21 +0300

    TomCat running successfully

--- src/main/java/ru/newrishman/library/config/WebAppInitializer.java
+++ src/main/java/ru/newrishman/library/config/WebAppInitializer.java
@@ -1,13 +1,9 @@
 package ru.newrishman.library.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
@@ -28,4 +24,5 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
     protected String[] getServletMappings() {
         return new String[]{"/"};
     }
+
 }
--- src/main/java/ru/newrishman/library/config/WebConfig.java
+++ src/main/java/ru/newrishman/library/config/WebConfig.java
@@ -3,8 +3,12 @@ package ru.newrishman.library.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 import ru.newrishman.library.service.AuthorService;
 import ru.newrishman.library.service.AuthorServiceImpl;
 import ru.newrishman.library.service.BookService;
@@ -15,12 +19,28 @@ import ru.newrishman.library.service.BookServiceImpl;
 @ComponentScan(value = "ru.newrishman.library")
 public class WebConfig implements WebMvcConfigurer {
 
-@Bean
-public AuthorService authorService(){
-    return new AuthorServiceImpl();
-}
-@Bean
-    public BookService bookService(){
-    return new BookServiceImpl();
-}
+    @Bean
+    public AuthorService authorService() {
+        return new AuthorServiceImpl();
+    }
+
+    @Bean
+    public BookService bookService() {
+        return new BookServiceImpl();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+
+        return viewResolver;
+    }
 }
